f0d02de05c27f9ee2f84e0369265a150e8935ed1
anvarbekergashev262@gmail.com <ergashsevanvarbek@gmail.com>
2023-08-02 19:50:38 +0500

    first commit

---
+++ 8# foydalanuvchi bilan muloqot.js
@@ -0,0 +1,12 @@
+"use strict" //Qatiy qoida bo'lib jsdagi eski kodlarni xato qilib ko'rsatati
+
+// const age = +prompt('Nechi yoshsan'); //String malumot turarini o'zgartirish uchun + qo'yilyapti
+// console.log(age)
+
+const favouriteColor = []
+
+favouriteColor[0] = prompt('Yoqtirgan rangingiz #1','');
+favouriteColor[1] = prompt('Yoqtirgan rangingiz #2','');
+favouriteColor[2] = prompt('Yoqtirgan rangingiz #3','');
+
+console.log(favouriteColor);
---
+++ operatorlar.js
@@ -0,0 +1,35 @@
+"use strict" //Qatiy qoida bo'lib jsdagi eski kodlarni xato qilib ko'rsatati
+
+//  console.log("Anvarbek"-"Ergashev"); 
+
+
+// Inkrement(++), decrement(--)
+
+// let incr = 10;
+// let decr = 10;
+
+// // incr++;
+// // decr--;
+// console.log(incr++);//pretiks
+// console.log(decr--);
+
+
+//console.log(10 % 3);
+
+// console.log(5 * 5 == 25);//Teklik belgisi ikkita tenglik belgisi
+
+
+// console.log(5 * 5 === "25"); // uchta tenglik ham qiymat ham ma'lumot turi bilan ham solishtiradi
+
+//&&-va(bu bitta false bolsa ham falseni qaytradi)    ||-yoki
+
+// const isAge = true;
+// const isClose = true;
+// const isChecked = false;
+
+// console.log(isAge && isClose && !isChecked);
+
+// console.log(!isChecked)
+
+
+// console.log(2 + 2 * 2 !== "8")
